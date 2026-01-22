ac690e84e061f5eb6b2a3bfdc2573e2145b1c0fc
hashir ahmed <ahmedhashir910@gmail.com>
2026-01-23 03:15:56 +0500

    Add files via upload

--- Data Engineer/Sql/day1.sql
+++ Data Engineer/Sql/day1.sql
@@ -14,3 +14,54 @@ id,
 first_name
 From customers
 where id>=2
+
+SELECT 
+ first_name,
+ country
+FROM customers
+WHERE country='Germany'
+
+/* The order of execution is 
+select(3)
+from(1)
+where (2)*/
+
+SELECT 
+ first_name,
+ country,
+ score
+FROM customers
+ORDER BY score DESC
+
+
+SELECT 
+ first_name,
+ country,
+ score
+FROM customers
+ORDER BY score DESC, country DESC
+-- ORDER BY CAN ALSO TAKE TWO VALUES ( NOT SHOWING EFFECT BECAUSE IT IS CONTRADICTING WITH THE FIRST VALUE --
+
+SELECT 
+	first_name,
+	score
+FROM customers
+WHERE  score > 350
+ORDER BY score DESC
+
+/* THE EXECUTION ORDER WILL BE
+SELECT                (4)
+	first_name,
+	score
+FROM customers        (1)
+WHERE  score > 350    (2)
+ORDER BY score DESC   (3)*/
+
+
+SELECT 
+	country,
+	avg(score) AS average,
+	COUNT(id) AS idcount
+FROM customers
+WHERE score > 500
+GROUP BY country
